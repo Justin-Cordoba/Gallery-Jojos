2363b5f372043194040064dede33fcb4042cf5f8
Justin-Cordoba <jcordobasevilla@gmail.com>
2024-08-08 15:11:02 -0600

    Gallery Jojos

---
+++ src/components/GalleryImage.jsx
@@ -0,0 +1,7 @@
+const GalleyImage = ({ image }) => {
+  return (
+    <div className={`${image} h-[500px] w-[300px] bg-cover bg-no-repeat bg-center hover:w-[500px] duration-[0.5s] ease-in-out grayscale hover:grayscale-0 transition-all`}/>
+  );
+};
+
+export default GalleyImage;
