8cc5d384fa7efad7e34351547527f4c5d20de505
Justin-Cordoba <jcordobasevilla@gmail.com>
2024-08-10 23:17:40 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import GalleyImage from "./components/GalleryImage";
 function App() {
   return (
     <div className="flex gap-4">
-      <GalleyImage image='bg-[url("2.jpg")]'/>
-      <GalleyImage image='bg-[url("1.png")]'/>
-      <GalleyImage image='bg-[url("2.jpg")]'/>
-      <GalleyImage image='bg-[url("1.png")]'/>
-      <GalleyImage image='bg-[url("2.jpg")]'/>
-     
+      <GalleyImage image='bg-[url("2.jpg")]' />
+      <GalleyImage image='bg-[url("1.png")]' />
+      <GalleyImage image='bg-[url("2.jpg")]' />
+      <GalleyImage image='bg-[url("1.png")]' />
+      <GalleyImage image='bg-[url("2.jpg")]' />
     </div>
   );
 }
 
 export default App;
-  
--- src/components/GalleryImage.jsx
+++ src/components/GalleryImage.jsx
@@ -1,6 +1,8 @@
 const GalleyImage = ({ image }) => {
   return (
-    <div className={`${image} h-[500px] w-[300px] bg-cover bg-no-repeat bg-center hover:w-[500px] duration-[0.5s] ease-in-out grayscale hover:grayscale-0 transition-all`}/>
+    <div
+      className={`${image} h-[500px] w-[300px] bg-cover bg-no-repeat bg-center hover:w-[500px] duration-[0.5s] ease-in-out grayscale hover:grayscale-0 transition-all`}
+    />
   );
 };
 
